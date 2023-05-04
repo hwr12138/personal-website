2183763330e726022ded37957e8e90418d271567
hrui119 <anson.rui@mail.utoronto.ca>
2023-05-04 12:57:46 -0600

    first commit

---
+++ src/data/projects.js
@@ -0,0 +1,42 @@
+// TODO Add a couple lines about each project
+const data = [
+  {
+    title: 'Boundary Breaker',
+    subtitle: 'Full-Stack Application',
+    image: '/images/projects/grocery.jpg',
+    date: '2021-11-20',
+    desc:
+      'Built a RESTful Django API that can process HTTP requests and communicate with a SQL database. '
+      + 'Developed a mobile application interface using Flutter that displays and navigates an in-store map '
+      + 'that allow users to quickly find their goods in a store.',
+  },
+  {
+    title: 'Handwriting Recognizer - Neural Network',
+    subtitle: 'Neural network that recognizes hand written digits',
+    image: '/images/projects/digit.jpg',
+    date: '2023-03-20',
+    desc:
+      'Trained a neural network to recognize hand-written digits with an accuracy of over 86%. '
+      + 'Implemented in C, decision guided by sigmoid activation and tuned using error back propagation.',
+  },
+  {
+    title: 'Cat and Mouse Game AI',
+    subtitle: 'A reinforcement learning model that plays the cat and mouse game',
+    image: '/images/projects/cat_mouse.jpg',
+    date: '2023-02-28',
+    desc:
+      'Trained a reinforcement learning algorithm to play the cat and mouse game in a maze.'
+      + 'Implemented using the Q-learning algorithm to optimize the action selection policy.',
+  },
+  {
+    title: 'Teammate Finder App',
+    subtitle: 'Full-Stack Application',
+    image: '/images/projects/team.jpg',
+    date: '2020-03-15',
+    desc:
+      'Built an app that suggests appropriate teammate based on how well they match each other\'s profile.'
+      + 'Led a team of 4 to plan and coordinate strategies under a tight timeline.',
+  },
+];
+
+export default data;
