d1bd9b46303e3779ecbf1e5563b863d2a2a63c8d
hrui119 <anson.rui@mail.utoronto.ca>
2023-05-04 21:38:15 -0600

    recommit

---
+++ src/data/resume/degrees.js
@@ -0,0 +1,10 @@
+const degrees = [
+  {
+    school: 'University of Toronto',
+    degree: 'Honours Bachelor of Science, Specialist in Computer Science (Software Engineering), Major in Statistics',
+    link: 'https://www.utoronto.ca/',
+    year: 2023,
+  },
+];
+
+export default degrees;
