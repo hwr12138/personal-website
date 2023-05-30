01010b731b1d8f3fbf5612519d5bfc98176795d6
hrui119 <anson.rui@mail.utoronto.ca>
2023-05-29 20:01:59 -0600

    small changes

--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -14,17 +14,17 @@
 const work = [
   {
     name: 'Scotiabank',
-    position: 'ML Engineer',
+    position: 'Software Engineer',
     url: 'https://www.scotiabank.com/ca/en/personal.html',
     startDate: '2022-01-01',
     endDate: '2022-08-27',
     summary: 'Worked with a team of engineers and data scientists to develop ML pipelines',
     highlights: [
-      'Developed the data analysis, feature engineering, and model training pipelines using Python, XGBoost, and LGBM, processed over 1M customers data, and improved its accuracy by 14%.',
-      'Implemented reusable pipelines for mortgage price optimization and feature engineering.',
-      'Conduct research on MOB segmentation and devise computational experiments to cluster data.',
+      'Developed the feature engineering, and ML model training pipelines using Python, XGBoost, and LGBM, processed over 1M customers data, and improved its accuracy by 14%.',
+      'Implemented reusable ML pipelines for mortgage price optimization and income estimator projects.',
       'Developed an Airflow data ingestion pipeline using Spark, Oracle SQL database, and Kubernetes that decreased data ingestion time by over 200%.',
-      'Delivered advanced statistical analysis to deepen business understanding and led a team tech talk.',
+      'Conduct research on MOB segmentation and devise computational experiments to cluster data.',
+      'Proactively engaged with stakeholders to design software architecture and led a team tech-talk.',
     ],
   },
   {
@@ -36,9 +36,9 @@ const work = [
     summary: 'Lead development of a series of websites, APIs, interactive web maps, and mobile apps',
     highlights: [
       'Led development of a series of websites and interactive web maps capable of displaying multiple layers of GIS and Geospatial data to visualize historical climate trends and key indicators.',
-      'Implemented a RESTful API that computes climate statistics using Java, Python, and R.',
+      'Implemented a RESTful Java API that computes climate statistics in agile development cycles.',
       'Developed a mobile application capable of collecting geolocation and climate data. The web maps actively display and integrate the collected data in real time.',
-      'Set up and maintained a web server and its connection to a MySQL database.',
+      'Maintained the capacity and performance of web servers, APIs, and databases.',
     ],
   },
 ];
